b90fb3119271ec7728a29b7c41e750714917b180
Hector <Hector@10.31.27.94>
2017-11-15 18:39:02 -0400

    Program makes a 200x200 JFrame with my last name

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,13 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("Breton");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setLocation(600, 300);
+		myFrame.setSize(200, 200);
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
+		//
 	}
 }
